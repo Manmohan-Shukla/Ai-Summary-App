d495d5d02eced3db36ed14ab7c25a7ba7d9371e2
Manmohan <manmohanshukla815@gmail.com>
2025-05-23 21:05:52 +0530

    added authentication

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -2,9 +2,9 @@
 import { FileText } from "lucide-react";
 import { Button } from "../ui/button";
 import NavLink from "./nav-link";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
-  const isLogged = false;
   return (
     <nav
       className="container flex items-center justify-between
@@ -16,26 +16,32 @@ export default function Header() {
             className="w-5 h-5 lg:w-8 lg:h-8 text-gray-900 hover:rotate-12
           transform transition duration-200 ease-in-out"
           />
-          <span className="font-extrabold lg:text-xl text-gray-900">Soomm</span>
+          <span className="font-extrabold lg:text-xl text-gray-900">
+            Summary
+          </span>
         </NavLink>
       </div>
       <div className=" flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
-        <NavLink href="/#pricing">pricing</NavLink>
-        {isLogged && <NavLink href="/#dashboard">your summaries</NavLink>}
+        <NavLink href="/#pricing">Pricing</NavLink>
+        <SignedIn>
+          <NavLink href="/dashboard">Your Summaries</NavLink>
+        </SignedIn>
       </div>
 
       <div className="flex lg:justify-end lg:flex-1">
-        {isLogged ? (
+        <SignedIn>
           <div className=" flex gap-2 items-center">
             <NavLink href="/#upload">upload a pdf</NavLink>
             <div>Pro</div>
-            <Button>User</Button>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-        ) : (
-          <div>
-            <NavLink href="/signin">signin</NavLink>
-          </div>
-        )}
+        </SignedIn>
+
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </div>
     </nav>
   );
